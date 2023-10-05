c37651fd2210b9cbab10ecc3fda4d0b5f78252e2
Kevin Hai Nam Pham <pham@tu-berlin.de>
2023-10-05 20:17:39 +0200

    implement didcomm message encryption [WIP]

--- src/comm/didcomm.js
+++ src/comm/didcomm.js
@@ -1,67 +1,65 @@
 import crypto from 'crypto';
 import base64url from 'base64url';
+import fs from 'fs';
+import { resolvePublicKeyWeb } from "./didweb.js"
 
-const recipientPublicKeyJWK = {
-  kty: "EC",
-  x: "2i-r7PMXK9r9fvUcBDpB9P0LLjsiLZHKmgLrGOW6wg0",
-  y: "84uUdh0RR7MhoDGO-6pKlHRjGLOB5z62QYI3GedOEU8",
-  crv: "secp256k1",
-  kid: "bZMbXJsSsCulRAUldmOfDKx8ykaFF_NPLuQIDkSUWjc",
-  use: "sig",
-  alg: "ES256K"
-}
-
-const recipientPrivateKeyJWK = {
-  kty: "EC",
-  x: "2i-r7PMXK9r9fvUcBDpB9P0LLjsiLZHKmgLrGOW6wg0",
-  y: "84uUdh0RR7MhoDGO-6pKlHRjGLOB5z62QYI3GedOEU8",
-  crv: "secp256k1",
-  d: "5UyZnZDArHNEAltbFRZ8KML9T3Vata9Juzcw0Sgv9Ac",
-  kid: "bZMbXJsSsCulRAUldmOfDKx8ykaFF_NPLuQIDkSUWjc",
-  use: "sig",
-  alg: "ES256K"
-}
-
-const recipientPublicKeyECDH = crypto.createECDH(recipientPublicKeyJWK.crv);
-recipientPublicKeyECDH.setPublicKey(Buffer.concat([
-  Buffer.from([0x04]), // Indicates that the key is in uncompressed format
-  base64url.toBuffer(recipientPublicKeyJWK.x),
-  base64url.toBuffer(recipientPublicKeyJWK.y)
-]));
-
-// Generate an ephemeral key pair
-const ephemeralECDH = crypto.createECDH(recipientPublicKeyJWK.crv);
-ephemeralECDH.generateKeys();
-
-// Compute the shared secret
-const sharedSecret = ephemeralECDH.computeSecret(recipientPublicKeyECDH.getPublicKey());
-
-// Encrypt the message with the shared secret
 const iv = crypto.randomBytes(16);
-const cipher = crypto.createCipheriv('aes-256-cbc', sharedSecret, iv);
 
-let encrypted = cipher.update('Hello, world!', 'utf8', 'hex');
-encrypted += cipher.final('hex');
+export function encryptMessage(recipientPublicKeyJWK) {
+  const recipientPublicKeyECDH = crypto.createECDH(recipientPublicKeyJWK.crv);
+  recipientPublicKeyECDH.setPublicKey(Buffer.concat([
+    Buffer.from([0x04]), // Indicates that the key is in uncompressed format
+    base64url.toBuffer(recipientPublicKeyJWK.x),
+    base64url.toBuffer(recipientPublicKeyJWK.y)
+  ]));
 
-// The encrypted message and the ephemeral public key are sent to the recipient
-const messageToRecipient = {
-  encrypted,
-  ephemeralPublicKey: ephemeralECDH.getPublicKey()
-};
+  // Generate an ephemeral key pair
+  const ephemeralECDH = crypto.createECDH(recipientPublicKeyJWK.crv);
+  ephemeralECDH.generateKeys();
+
+  // Compute the shared secret
+  const sharedSecret = ephemeralECDH.computeSecret(recipientPublicKeyECDH.getPublicKey());
+
+  // Encrypt the message with the shared secret
+  const cipher = crypto.createCipheriv('aes-256-cbc', sharedSecret, iv);
+
+  let encrypted = cipher.update('Hello, world!', 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+
+  // The encrypted message and the ephemeral public key are sent to the recipient
+  return {
+    encrypted,
+    ephemeralPublicKey: ephemeralECDH.getPublicKey()
+  };
+}
+
+export function decryptMessage(recipientPrivateKeyJWK, message) {
+  const recipientPrivateKeyECDH = crypto.createECDH(recipientPrivateKeyJWK.crv);
+  recipientPrivateKeyECDH.setPrivateKey(Buffer.from(recipientPrivateKeyJWK.d, 'base64'));
+
+  // Compute the shared secret
+  const sharedSecret = recipientPrivateKeyECDH.computeSecret(message.ephemeralPublicKey);
+
+  // Decrypt the message with the shared secret
+  const decipher = crypto.createDecipheriv('aes-256-cbc', sharedSecret, iv);
+
+  let decrypted = decipher.update(message.encrypted, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
+  return decrypted
+}
+
+/*
+const thing2PublicKeyJWK = await resolvePublicKeyWeb("did:web:phamkv.github.io:things:thing2")
+
+const messageToRecipient = encryptMessage(thing2PublicKeyJWK)
 console.log(messageToRecipient)
 
 /////////////
 
-const recipientPrivateKeyECDH = crypto.createECDH(recipientPrivateKeyJWK.crv);
-recipientPrivateKeyECDH.setPrivateKey(Buffer.from(recipientPrivateKeyJWK.d, 'base64'));
+const privateJWKObj = fs.readFileSync("src/comm/test/private2.json", 'utf8');
+const thing2PrivateKeyJWK = JSON.parse(privateJWKObj);
 
-// Compute the shared secret
-const sharedSecret2 = recipientPrivateKeyECDH.computeSecret(messageToRecipient.ephemeralPublicKey);
+const decryptedMessage = decryptMessage(thing2PrivateKeyJWK, messageToRecipient)
 
-// Decrypt the message with the shared secret
-const decipher = crypto.createDecipheriv('aes-256-cbc', sharedSecret2, iv);
-
-let decrypted = decipher.update(messageToRecipient.encrypted, 'hex', 'utf8');
-decrypted += decipher.final('utf8');
-
-console.log(decrypted)
+console.log(decryptedMessage)
+*/
---
+++ src/comm/didweb.js
@@ -0,0 +1,20 @@
+import { Resolver } from 'did-resolver'
+import { getResolver } from 'web-did-resolver'
+
+async function resolveDIDWeb(url) {
+  const webResolver = getResolver()
+  const resolver = new Resolver({
+    ...webResolver
+  })
+  
+  return await resolver.resolve(url).then(result => result["didDocument"])
+}
+
+export async function resolvePublicKeyWeb(url) {
+  const didDocument = await resolveDIDWeb(url)
+    
+  return didDocument["verificationMethod"].find(method => method.id === didDocument["authentication"][0]).publicKeyJwk
+}
+
+// const doc = await resolvePublicKeyWeb('did:web:phamkv.github.io:things:thing1')
+// console.log(doc)
